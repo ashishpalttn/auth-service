050dc040091bf5a47c7501f96a9586ac817df2ee
Ashish <terminal777888@gmail.com>
2025-10-01 04:16:03 +0530

    refactor: update default properties in getVendorResponse to include 'name'

--- src/utils/util.js
+++ src/utils/util.js
@@ -47,7 +47,7 @@ function getClientResponse(user, properties = ['name', 'mobile']) {
     return responseObj;
 }
 
-function getVendorResponse(user, properties=['otpExpireTime','otp']) {
+function getVendorResponse(user, properties=['name','otpExpireTime','otp']) {
     // Remove only specified properties, keep the rest
     if (!user || !Array.isArray(properties)) return {};
     const result = { ...user };
